e4da3d1b243237d27012b9edf8861a93d52e3032
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-10-25 22:28:29 -0300

    chore: added setup postgress connection

--- api/videos/settings.py
+++ api/videos/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'videos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
