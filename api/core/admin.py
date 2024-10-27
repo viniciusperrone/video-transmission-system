af3a6703bdd9842f32209f8be6bbf6029de593b1
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-10-27 00:29:42 -0300

    feat: updated admin.py

--- api/core/admin.py
+++ api/core/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import csrf_protect_m
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.urls import path, reverse
 from django.utils.html import format_html
+
+from core.form import VideoChunkUploadForm
+from core.services import VideoService
 
 from .models import Video, Tag
 
@@ -18,12 +23,30 @@ class VideoAdmin(admin.ModelAdmin):
         urls = super().get_urls()
         custom_urls = [
             path('<int:id>/upload-video', self.upload_video, name='core_video_upload'),
+            path('<int:id>/finish-upload-video', self.finish_upload_video, name='core_video_upload_finish')
         ]
 
         return custom_urls + urls
 
+    @csrf_protect_m
     def upload_video(self, request, id):
-        return render(request, 'admin/core/upload_video.html')
+        if request.method == 'POST':
+            form = VideoChunkUploadForm(request.POST, request.FILES)
+
+            if not form.is_valid():
+                return JsonResponse({ 'error': form.errors }, status=400)
+            
+            VideoService().process_upload(
+                video_id=id,
+                chunk_index=form.cleaned_data['chunkIndex'],
+                chunk=form.cleaned_data['chunk'].read()
+            )
+
+        context = dict(
+            id=id
+        )
+
+        return render(request, 'admin/core/upload_video.html', context)
     
     def redirect_to_upload(self, obj: Video):
         url = reverse('admin:core_video_upload', args=[obj.id])
@@ -31,6 +54,9 @@ class VideoAdmin(admin.ModelAdmin):
         return format_html(f'<a href="{url}">Upload</a>')
     
     redirect_to_upload.short_description = 'Upload'
+
+    def finish_upload_video(self, request, id):
+        pass 
 
 
 # Register your models here.
